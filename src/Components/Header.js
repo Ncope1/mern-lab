77ec02eb017480b06ce9fcde4ea74d2b7254a3ca
Natalee C <COPENATALEE@ME.COM>
2018-01-22 22:14:36 -0500

    wrote code for Header.js component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 
 const Header = () => (
-    <nav>
-        <div>
-            
-        </div>
-    </nav>
+<div className="card-panel purple accent-1">
+<nav className="purple accent-1">
+    <ul className="left hide-on-med-and-down">
+        <li>
+            <a className="navbar-brand" href="/">Home</a>
+        </li>
+        <li>
+            <a href="/routines">Routines</a>
+        </li>
+
+    </ul>
+</nav>
+</div>
 )
 
 export default Header
